4cfacc2d2b3b6890bf429aea7351b72074dc6a82
andreeliasgv <andrevila2016@hotmail.com>
2025-11-20 18:38:12 -0300

    update JAVA/Iphone/src/main/Main.java

--- JAVA/Iphone/src/main/Main.java
+++ JAVA/Iphone/src/main/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         Iphone iphone = new Iphone();
-        
+
         iphone.selecionarMusica("Back in Black");
         iphone.tocar();
         iphone.pausar();
-        
+
         iphone.ligar("99462-4537");
         iphone.atender();
         iphone.iniciarCorreioVoz();
-        
+
         iphone.exibirPagina("https://www.google.com");
         iphone.adicionarNovaAba();
         iphone.atualizarPagina();
